5aea0495fff39d316979a3377540c8daf2f1ee8a
Mohd Mudassir Ansari <mudassirimranansari@gmail.com>
2024-09-22 21:38:10 +0530

    minor changes

--- app.py
+++ app.py
@@ -4,26 +4,18 @@ from tensorflow.keras.models import load_model
 import numpy as np
 from PIL import Image
 
-# Load the trained model
 model = load_model('models/mnist_model.h5')
 
 def preprocess_image(image):
-    # Resize the image to 28x28 pixels
     image = image.resize((28, 28))
-    # Convert to grayscale
     image = image.convert('L')
-    # Convert to numpy array and normalize
     image = np.array(image).astype('float32') / 255
-    # Reshape to (1, 28, 28, 1) for model input
     image = image.reshape(1, 28, 28, 1)
     return image
 
 def predict_digit(image):
-    # Preprocess the image
     processed_image = preprocess_image(image)
-    # Make prediction
     prediction = model.predict(processed_image)
-    # Get the predicted digit
     digit = np.argmax(prediction)
     return digit
 
